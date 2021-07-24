e426b1d201ab7672bd0fb4a5885ccb4e65f72d78
samstha922 <slevin.shretha@gmail.com>
2021-07-25 07:43:23 +1000

    All conflicts solved

---
+++ es6.js
@@ -0,0 +1,107 @@
+// const pi = 3.14
+// //  pi =20;
+// console.log(pi);
+// function calcArea(r){
+//     // takes the local scope of pi value if u declare const here
+//     // const pi = 10
+//     console.log(pi*r*r);
+// }
+// calcArea(5);
+
+// var x =10;
+// if(x>5){
+//     //value of x will be 5 globally
+//     // var x =5; 
+     
+//     let x=5;
+//     console.log('inside:'+x);
+// }
+// console.log('outside:'+x);
+
+// var items = document.getElementsByTagName('li');
+// // let solves the issues of var which will be declared globally as below: the value of x is always 4 below.
+// // for (var x =0;x<items.length;x++){}
+
+// for (let x =0;x<items.length;x++){
+//     items[x].onclick = function(){
+//         console.log(x);
+//     }
+// } 
+
+// var num1 = [1,2,3];
+// // spread operator spreads the data into individual components
+// var num2=[...num1, 4,5,6];
+// console.log(num2)
+// function adddNum1(a,b,c){
+//     console.log(a+b+c);
+// }
+// adddNum1(...num1);
+
+// // //------------------------------------------------------
+// // a way to define method without fxn name 
+// var somefxn = function(x){
+//     return(`some function with ${x}`);
+// }
+// smf = somefxn(5);
+// console.log(smf);
+
+// // same thing as above with arrow fxn in deprecated form
+// var Arrowfxn = y => console.log(`arrow fxn with ${y}`);
+// Arrowfxn(6);
+
+
+// // //------------------------------------------
+// window.onload=function(){
+    
+//     // // //backticks renders the data the way it is 
+//     // let name = 'ream';     
+//     // var temp = `My name is ${name} and age is ${5+3}`;
+//     // console.log(temp);
+//     // function chops(x){
+//     //     window.setInterval(function (){
+//     //         if(x>0){
+//     //             // overriding the 'name' variable
+//     //             // let name="Sam" 
+//     //             console.log(`${name} has been chopped ${x} times`);
+//     //             x--;
+//     //         }
+//     //     },1000);
+//     // }
+//     // chops(5);
+
+//     // // -------defining fxn with objects
+//     var ninja = {
+//         name :  'Sajjan',
+//         chop : function(x){
+//             //incase if u dont want to use arrow fxn. then define that and use it
+//             // var that = this;
+//             window.setInterval(()=>{
+//                if(x>0){
+//                 console.log(`${this.name} has been chopped ${x} times`);  
+//                 x--;
+//                } 
+//             },1000)
+//         },
+//     }
+//     //console.log(typeof (ninja));
+//     ninja.chop(5);
+// }
+
+
+// //-----------------generators----------------
+window.onload = function(){
+    // * converts to generator; generator needs iterator
+    function* gen(){
+        yield 'pear';
+        yield 'apple'; // yield: pause ; js runs from right to left
+        yield 'banana';
+        console.log('all done');
+    }
+
+    // this is iterator for generator
+    var myGen = gen();
+    //this is like a play button for the code
+    console.log(myGen.next());
+    //console.log(myGen.next());
+}
+
---
+++ object.js
@@ -0,0 +1,120 @@
+//   // object creation and object literals
+// var userOne= {
+//     email: 'slevinshrestha@gmail.com',
+//     name: 'Sam',
+//     login(){
+//         console.log(this.email+ 'has logged in')
+//     },
+//     logout(){
+//         console.log(this.email+ 'has logged out')
+//     }
+// }
+// userOne.name = "Sampresh Stha";
+// // can update the properties both ways
+// userOne['email'] = "samsam@SpeechGrammarList.com";
+
+// // create new properties and methods in object
+// userOne.age = 25;
+// userOne['onlineStatus'] = 'active';
+// // also can create a method like this
+// userOne.status = function(){
+//     console.log(userOne.email+ 'is'+ userOne.onlineStatus);
+// } 
+//   // --------------------------------------------------------
+
+
+// constructor
+// class User{
+//     constructor(name, email){
+//         this.name = name; 
+//         this.email = email;
+//         this.score = 0;
+//     }
+//     login(){
+//         console.log(this.email+ 'has logged in');
+//         return this;
+//     }
+//     logout(){
+//         console.log(this.email+ 'has logged out')
+//         return this
+//     }
+//     updateScore(){
+//         this.score++;
+//         console.log('Score for '+this.email+ ' is now '+this.score);
+//         //  returning the instance of the object
+//         return this; 
+//     }
+// }
+// // Inheritance -- extends
+// class Admin extends User{
+//     deleteUser(user){
+//         users = users.filter(u => {
+//             return u.email != user.email
+//         })
+//     }
+// }
+
+// var userA = new User('s1','s1@gmail.com');
+// var userB = new User('s2','s2@gmail.com');
+// // admin object has access to deleteUser method
+// var admin =new Admin('s3','s3@gmail.com');
+
+// // creating array of objects
+// var users  = [userA, userB, admin];
+
+// // console.log('Total users');
+// // console.log(users);
+
+// // filtering data to give admin roles
+// admin.deleteUser(userB);
+// console.log(users);
+
+// // method chaining needs the return this to occur
+// userA.login().updateScore().updateScore().logout();
+
+// //----------------------------------------------------------------------
+
+// Making class and constructors without using class keyoword.....using prototype fucntion
+
+function User(email, name){
+    this.email = email;
+    this.name = name;
+    this.online= false;
+}
+User.prototype.login = function(){
+    this.online =true;
+    console.log(this.email+" has logged in");
+}
+User.prototype.logout=function(){
+    this.online =false;
+    console.log(this.email+" has logged out");
+}
+// ...args is spread operator....place is parameter into an array
+function Admin(...args){
+    // console.log(args);
+
+    // getting the reference of this from User class and connecting to args
+    User.apply(this,args);
+    this.role='super_admin';
+}
+
+// inherit methods: like login() from user methods
+Admin.prototype = Object.create(User.prototype)
+
+// adding new method to Admin
+Admin.prototype.deleteUser = function(user){
+    users = users.filter(u=>{
+        return u.email != user.email
+    })
+}
+
+var userOne = new User('AA',"aa@gmail.com");
+var userTwo = new User('BB',"bb@gmail.com");
+var admin = new Admin('Sam','sam@admin.com')
+// userOne.login();
+var users = [userOne,userTwo,admin];
+//console.log(admin);
+// admin.deleteUser(userOne);
+// console.log(users);
+
+
